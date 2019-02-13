e5760e125d8adf5993e47845a7abca7671352e1d
Annika Traner <annika.traner@gmail.com>
2019-02-12 20:56:23 -0500

    Update friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,28 +1,28 @@
 var friends = [
 	{
-		name: "Charlie",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/0/0a/Charlie_%289%29.jpg",
-		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+		name: "Maria",
+		photo: "https://avatars2.githubusercontent.com/u/40623184?s=460&v=4",
+		scores: [3, 2, 4, 2, 4, 2, 2, 3, 5, 5]
 	},
 	{
-		name: "Sweet Dee",
-		photo: "https://vignette2.wikia.nocookie.net/itsalwayssunny/images/f/f3/Square-Dee-Cheesy.jpg",
-		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+		name: "Hailey",
+		photo: "https://avatars0.githubusercontent.com/u/43506564?s=460&v=4",
+		scores: [4, 3, 2, 4, 2, 3, 1, 4, 4, 4]
 	},
 	{
-		name: "Dennis",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/1/13/Dennis_%281%29.jpg",
-		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+		name: "Annika",
+		photo: "https://avatars1.githubusercontent.com/u/43939292?s=460&v=4",
+		scores: [5, 5, 1, 1, 5, 1, 5, 1, 1, 3]
 	},
 	{
-		name: "Mac",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/5/5f/Mac_-_Season_8.jpg",
-		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+		name: "David",
+		photo: "https://avatars0.githubusercontent.com/u/43341167?s=460&v=4",
+		scores: [2, 1, 3, 5, 3, 5, 3, 5, 2, 2]
 	},
 	{
-		name: "Frank",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/f/fe/Season_7_-_Frank.jpg",
-		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+		name: "Laura",
+		photo: "https://avatars1.githubusercontent.com/u/43973664?s=460&v=4",
+		scores: [1, 4, 5, 4, 1, 4, 4, 2, 3, 1]
 	}
 ];
 
